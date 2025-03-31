c47368d172d02e90ac04dd059e1844b509da5b5b
hanndoddi <hanndoddi@gmail.com>
2025-03-31 01:22:05 +0000

    removing old kickstart files

--- .config/nvim/lua/custom/config/markdown.lua
+++
@@ -1,8 +0,0 @@
-vim.api.nvim_create_autocmd("FileType", {
-    pattern = { "markdown", "org", "text" },
-    callback = function()
-        vim.api.nvim_buf_set_keymap(0, "i", "<C-x>",
-            [[<Esc><leader>x a]],
-            { noremap = true, silent = true })
-    end,
-})
