49704cd39f3df0a966e242a6ffa392ed1729930f
hanndoddi <hanndoddi@gmail.com>
2025-03-31 13:03:12 +0000

    linting and git intergration

--- .config/nvim/lua/hanndoddi/plugins/lsp/mason.lua
+++ .config/nvim/lua/hanndoddi/plugins/lsp/mason.lua
@@ -46,6 +46,8 @@ return {
         "stylua", -- lua formatter
         "isort", -- python formatter
         "black", -- python formatter
+        "pylint", -- python linter
+        "eslint_d", -- js linter
       },
     })
   end,
--- .config/nvim/lua/hanndoddi/plugins/which-key.lua
+++ .config/nvim/lua/hanndoddi/plugins/which-key.lua
@@ -1,36 +1,36 @@
 return {
-    "folke/which-key.nvim",
-    event = "VeryLazy",
-    init = function()
-      vim.o.timeout = true
-      vim.o.timeoutlen = 500
-    end,
-    opts = {
-      -- your configuration comes here
-      -- or leave it empty to use the default settings
-      -- refer to the configuration section below
-      vim.cmd([[
+  'folke/which-key.nvim',
+  event = 'VeryLazy',
+  init = function()
+    vim.o.timeout = true
+    vim.o.timeoutlen = 500
+  end,
+  opts = {
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+    vim.cmd [[
         highlight WhichKeyGroup     guifg=#ffaa00 guibg=NONE
-        ]])
-    },
-    {
-      "echasnovski/mini.icons", -- Add this line to install mini.icons
-    },
-  }
+        ]],
+  },
+  {
+    'echasnovski/mini.icons', -- Add this line to install mini.icons
+  },
+}
 
-  --{
-  --  "folke/which-key.nvim",
-  --  opts = function(_, opts)
-  --    local icons = require("mini.icons")
-  --    opts.icons = {
-  --      breadcrumb = icons.ui.ChevronRight,
-  --      separator = icons.ui.ChevronRight,
-  --      group = icons.ui.Circle,
-  --    }
-  --    return opts
-  --  end,
-  --}
+--{
+--  "folke/which-key.nvim",
+--  opts = function(_, opts)
+--    local icons = require("mini.icons")
+--    opts.icons = {
+--      breadcrumb = icons.ui.ChevronRight,
+--      separator = icons.ui.ChevronRight,
+--      group = icons.ui.Circle,
+--    }
+--    return opts
+--  end,
+--}
 
+-- highlight WhichKeyDesc      guifg=#6A7DE9 guibg=NONE
+--   highlight WhichKey          guifg=#00ff00 guibg=NONE
 
-  -- highlight WhichKeyDesc      guifg=#6A7DE9 guibg=NONE
-  --   highlight WhichKey          guifg=#00ff00 guibg=NONE
