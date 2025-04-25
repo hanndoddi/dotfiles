77e0012ce3682e5133194b21f19ace02b480ba3a
hanndoddi <hanndoddi@gmail.com>
2025-04-25 23:28:52 +0000

    startup time addjust could break removed bufferline

--- .config/nvim/lua/hanndoddi/plugins/lsp/mason.lua
+++ .config/nvim/lua/hanndoddi/plugins/lsp/mason.lua
@@ -1,5 +1,7 @@
 return {
   "williamboman/mason.nvim",
+  cmd = { "Mason", "MasonInstall", "MasonUpdate", "MasonLog" },
+
   dependencies = {
     "williamboman/mason-lspconfig.nvim",
     "WhoIsSethDaniel/mason-tool-installer.nvim",
