504db171b59f9a974394184f40cef41abea5645e
hanndoddi <hanndoddi@gmail.com>
2025-04-25 22:17:57 +0000

    plugin config formatt noise zen and kitty config

--- .config/nvim/lua/hanndoddi/plugins/noise-nvim.lua
+++ .config/nvim/lua/hanndoddi/plugins/noise-nvim.lua
@@ -1,16 +1,33 @@
---- lazy.nvim
 return {
   'folke/noice.nvim',
   event = 'VeryLazy',
   opts = {
-    -- add any options here
+    lsp = {
+      override = {
+        ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+        ["vim.lsp.util.stylize_markdown"] = true,
+        ["cmp.entry.get_documentation"] = true,
+      },
+    },
+    presets = {
+      -- Optional: enable helpful default UIs
+      long_message_to_split = true,
+    },
   },
   dependencies = {
-    -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
     'MunifTanjim/nui.nvim',
-    -- OPTIONAL:
-    --   `nvim-notify` is only needed, if you want to use the notification view.
-    --   If not available, we use `mini` as the fallback
     'rcarriga/nvim-notify',
   },
+  config = function(_, opts)
+    -- Setup notify with a background color
+    require("notify").setup({
+      background_colour = "#000000",
+      merge_duplicates = true, -- resolves the warning about missing field
+    })
+
+    -- Setup noice with options
+    require("noice").setup(opts)
+
+    vim.keymap.set("n", "<leader>fn", "<cmd>Telescope notify<CR>", { desc = "Telescope Notify" })
+  end,
 }
--- .config/nvim/lua/hanndoddi/plugins/zen-mode.lua
+++ .config/nvim/lua/hanndoddi/plugins/zen-mode.lua
@@ -1,12 +1,7 @@
 return {
   'folke/zen-mode.nvim',
+  event = 'VeryLazy',
   opts = {
-
-    on_open = function()
-      print 'Zen Mode opened'
-      print('TERM:', vim.env.TERM)
-      print('KITTY_LISTEN_ON:', vim.env.KITTY_LISTEN_ON)
-    end,
     window = {
       backdrop = 0.95,
       width = 100,
@@ -23,13 +18,13 @@ return {
         ruler = false,
         showcmd = false,
       },
-      twilight = { enabled = false }, -- set to true if using twilight.nvim
-      gitsigns = { enabled = false },
-      tmux = { enabled = false },
-    },
-    kitty = {
-      enabled = true, -- this is key!
-      font = '+9', -- increase font size
+      twilight  = { enabled = false }, -- enable if using twilight.nvim
+      gitsigns  = { enabled = false },
+      tmux      = { enabled = false },
+      kitty     = {
+        enabled = true,
+        font = '+4',
+      },
     },
   },
   keys = {
