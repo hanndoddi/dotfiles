1ab444cf177a781618f4b345339a44853353dbde
hanndoddi <hanndoddi@gmail.com>
2025-05-06 19:55:24 +0000

    which key cleanup and added yazi function to zshrc

--- .config/nvim/lua/hanndoddi/plugins/which-key.lua
+++ .config/nvim/lua/hanndoddi/plugins/which-key.lua
@@ -1,59 +1,51 @@
-
 return {
   'folke/which-key.nvim',
   event = 'VeryLazy',
+  opts = {},
   init = function()
     vim.o.timeout = true
     vim.o.timeoutlen = 500
   end,
 
-opts = {
-  plugins = {
-    spelling = true,
-  },
-  filters = {
-    ignore_missing = true,
-  },
-  triggers_blacklist = {
-    n = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" },
-  },
-},
   config = function()
     vim.cmd [[
       highlight WhichKeyGroup guifg=#ffaa00 guibg=NONE
     ]]
 
-    local present, wk = pcall(require, "which-key")
+    local present, wk = pcall(require, 'which-key')
     if present then
       wk.add {
-        { "<leader>c", group = 'Code Action' },
-        { "<leader>h", group = 'Git' },
-        { "<leader>n", group = 'Clear Search Highlight' },
-        { '<leader>o', group = 'Org', icon = ""},
-        { "<leader>r", group = 'LSP', icon = "" },
-        { "<leader>w", group = 'Session' },
-        { '<leader>i', group = 'Embedded', icon = ""},
-        { '<leader>ip', group = 'PlatformIO', icon = ""},
-        { '<leader>s', group = 'Splits', icon = ""},
-        { "<leader>z", name = 'Relax!' },
-        { "<leader>1", hidden = true},
-        { "<leader>2", hidden = true},
-        { "<leader>3", hidden = true},
-        { "<leader>4", hidden = true},
-        { "<leader>5", hidden = true},
-        { "<leader>6", hidden = true},
-        { "<leader>7", hidden = true},
-        { "<leader>8", hidden = true},
-        { "<leader>9", hidden = true},
-        { "<leader>0", hidden = true},
-        { "<leader>b", group = "buffers", expand = function()
-        return require("which-key.extras").expand.buf()
-        end
+        { '<leader>c', group = 'Code Action' },
+        { '<leader>h', group = 'Git' },
+        { '<leader>n', group = 'Clear Search Highlight' },
+        { '<leader>o', group = 'Org', icon = '' },
+        { '<leader>r', group = 'LSP', icon = '' },
+        { '<leader>w', group = 'Session' },
+        { '<leader>i', group = 'Embedded', icon = '' },
+        { '<leader>ip', group = 'PlatformIO', icon = '' },
+        { '<leader>s', group = 'Splits', icon = '' },
+        { '<leader>z', name = 'Relax!' },
+        { '<leader>1', hidden = true },
+        { '<leader>2', hidden = true },
+        { '<leader>3', hidden = true },
+        { '<leader>4', hidden = true },
+        { '<leader>5', hidden = true },
+        { '<leader>6', hidden = true },
+        { '<leader>7', hidden = true },
+        { '<leader>8', hidden = true },
+        { '<leader>9', hidden = true },
+        { '<leader>0', hidden = true },
+        {
+          '<leader>b',
+          group = 'buffers',
+          expand = function()
+            return require('which-key.extras').expand.buf()
+          end,
         },
       }
     end
   end,
   dependencies = {
     { 'echasnovski/mini.icons', lazy = true },
-  }
+  },
 }
--- .zshrc
+++ .zshrc
@@ -168,7 +168,14 @@ up() {
     d=$(echo $d | sed 's/^\///')
     cd ${d:-..}
 }
-
+function y() {
+	local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+	yazi "$@" --cwd-file="$tmp"
+	if cwd="$(command cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
+		builtin cd -- "$cwd"
+	fi
+	rm -f -- "$tmp"
+}
 # =============================================
 # 12. Other Aliases
 # =============================================
