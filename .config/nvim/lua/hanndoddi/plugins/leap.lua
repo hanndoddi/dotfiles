3502f6e31bb921befbafb53a07d9ca6b0cf1d607
hanndoddi <hanndoddi@gmail.com>
2025-12-14 16:56:56 +0000

    nvim update synch and fixing leap

--- .config/nvim/lua/hanndoddi/plugins/leap.lua
+++ .config/nvim/lua/hanndoddi/plugins/leap.lua
@@ -1,7 +1,18 @@
+
 return {
   'ggandor/leap.nvim',
   event = 'VeryLazy',
   config = function()
-    require('leap').add_default_mappings()
+    local leap = require('leap')
+
+    -- OPTIONAL but recommended (preview filter for cleaner UX)
+    leap.opts.preview = true
+
+    -- Recommended default mappings:
+    vim.keymap.set({ 'n', 'x', 'o' }, 's', '<Plug>(leap-forward)')
+    vim.keymap.set({ 'n', 'x', 'o' }, 'S', '<Plug>(leap-backward)')
   end,
 }
+
+
+
