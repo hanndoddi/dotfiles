d17d917d1380be61aab75a075a3b5fc122ce5dec
hanndoddi <hanndoddi@gmail.com>
2026-03-16 00:06:35 +0000

    nvim telescope config update

--- .config/nvim/lua/hanndoddi/plugins/telescope.lua
+++ .config/nvim/lua/hanndoddi/plugins/telescope.lua
@@ -6,6 +6,16 @@ return {
     { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
     'nvim-tree/nvim-web-devicons',
   },
+  cmd = { 'Telescope' },
+  keys = {
+    { '<leader>ff', '<cmd>Telescope find_files<cr>',  desc = 'Fuzzy find files in cwd' },
+    { '<leader>fr', '<cmd>Telescope oldfiles<cr>',    desc = 'Fuzzy find recent files' },
+    { '<leader>fs', '<cmd>Telescope live_grep<cr>',   desc = 'Find string in cwd' },
+    { '<leader>fc', '<cmd>Telescope grep_string<cr>', desc = 'Find string under cursor in cwd' },
+    { '<C-p>',      '<cmd>Telescope find_files<cr>',  desc = 'Find Files' },
+    { '<leader>fg', '<cmd>Telescope live_grep<cr>',   desc = 'Live Grep' },
+    { '<leader><leader>', '<cmd>Telescope buffers<cr>', desc = 'Telescope buffers' },
+  },
   config = function()
     local telescope = require 'telescope'
     local actions = require 'telescope.actions'
@@ -15,8 +25,8 @@ return {
         path_display = { 'smart' },
         mappings = {
           i = {
-            ['<C-k>'] = actions.move_selection_previous, -- move to prev result
-            ['<C-j>'] = actions.move_selection_next, -- move to next result
+            ['<C-k>'] = actions.move_selection_previous,
+            ['<C-j>'] = actions.move_selection_next,
             ['<C-q>'] = actions.send_selected_to_qflist + actions.open_qflist,
           },
         },
@@ -24,29 +34,6 @@ return {
     }
 
     telescope.load_extension 'fzf'
-
-    -- set keymaps
-    local keymap = vim.keymap -- for conciseness
-
-    keymap.set('n', '<leader>ff', '<cmd>Telescope find_files<cr>', { desc = 'Fuzzy find files in cwd' })
-    keymap.set('n', '<leader>fr', '<cmd>Telescope oldfiles<cr>', { desc = 'Fuzzy find recent files' })
-    keymap.set('n', '<leader>fs', '<cmd>Telescope live_grep<cr>', { desc = 'Find string in cwd' })
-    keymap.set('n', '<leader>fc', '<cmd>Telescope grep_string<cr>', { desc = 'Find string under cursor in cwd' })
-  --  vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = '[S]earch [H]elp' })
-  --  keymap.set('n', '<leader>fk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-  --  vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
-  --  vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
-  --  keymap.set('n', '<leader>fw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-  --  keymap.set('n', '<leader>fg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
-  --  keymap.set('n', '<leader>fd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
-  --  vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
-  --  vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
-  --  keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
-  local builtin = require("telescope.builtin")
-      vim.keymap.set("n", "<C-p>", builtin.find_files, {desc = 'Find Files'})
-      vim.keymap.set("n", "<leader>fg", builtin.live_grep, {desc = 'Live Grep'})
-      vim.keymap.set("n", "<leader><leader>", builtin.buffers, {desc = 'Telescope buff'})
-
 
     local present, wk = pcall(require, 'which-key')
     if present then
@@ -56,4 +43,3 @@ return {
     end
   end,
 }
-
